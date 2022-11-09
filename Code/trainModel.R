4f95202c96e5cd4bc4d41a63453a6caa1824a754
I-finn <52965485+I-finn@users.noreply.github.com>
2022-11-09 13:49:57 +0000

    trainModel

---
+++ Code/trainModel.R
@@ -0,0 +1,39 @@
+#trainModel.R
+
+
+# detach all libraries
+detachAllPackages <- function() {
+  basic.packages <- c("package:stats", "package:graphics", "package:grDevices", "package:utils", "package:datasets", "package:methods", "package:base")
+  package.list <- search()[ifelse(unlist(gregexpr("package:", search()))==1, TRUE, FALSE)]
+  package.list <- setdiff(package.list, basic.packages)
+  if (length(package.list)>0)  for (package in package.list) detach(package,  character.only=TRUE)
+}
+detachAllPackages()
+
+# load libraries
+pkgTest <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[,  "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg,  dependencies = TRUE)
+  sapply(pkg,  require,  character.only = TRUE)
+}
+
+# load necessary packages
+lapply(c("ggplot2", "stargazer", "tidyverse", "stringr", "broom"),  pkgTest)
+
+# function to save output to a file that you can read in later to your docs
+output_stargazer <- function(outputFile, appendVal=TRUE, ...) {
+  output <- capture.output(stargazer(...))
+  cat(paste(output, collapse = "\n"), "\n", file=outputFile, append=appendVal)
+}
+
+
+# set working directory to current parent folder
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+
+###########################################
+
+
+dat <- readRDS("../Data/train.rds")
+View(dat)
